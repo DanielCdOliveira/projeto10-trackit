11d93d7ce4b4d57c33c0b6ac5fd982656028775f
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-03-29 15:36:17 -0300

    Routes Creation

---
+++ src/Components/App/index.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Start from "../Start";
+import Register from "../Register";
+import Habits from "../Habits";
+import Today from "../Today";
+import History from "../History";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />}></Route>
+        <Route path="/cadastro" element={<Register />}></Route>
+        <Route path="/habitos" element={<Habits />}></Route>
+        <Route path="/hoje" element={<Today />}></Route>
+        <Route path="/historico" element={<History />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Today/index.jsx
@@ -0,0 +1,5 @@
+function Today() {
+  return <h1>Hoje</h1>;
+}
+
+export default Today;
